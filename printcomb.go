959063d1e98518c4af35d26dc715dc982f795a1c
vomolo <valentineomolo1@gmail.com>
2023-10-26 16:42:48 +0300

    neg

--- printcomb.go
+++ printcomb.go
@@ -10,8 +10,8 @@ func PrintComb() {
 				z01.PrintRune(j)
 				z01.PrintRune(k)
 				if i != '7' {
-					zo1.PrintRune(',')
-					zo1.PrintRune(' ')
+					z01.PrintRune(',')
+					z01.PrintRune(' ')
 				}
 			}
 		}
