ac42b8f57a269d5d1234cdb0a5723390517e9c61
vomolo <valentineomolo1@gmail.com>
2023-10-26 16:34:46 +0300

    fffs

--- printcomb.go
+++ printcomb.go
@@ -3,18 +3,18 @@ package main
 import "github.com/01-edu/z01"
 
 func PrintComb() {
-    for i := '0'; i <= '7'; i++ {
-        for j := i + 1; j <= '8'; j++ {
-            for k := j + 1; k <= '9'; k++ {
-                z01.PrintRune(i)
+	for i := '0'; i <= '7'; i++ {
+		for j := i + 1; j <= '8'; j++ {
+			for k := j + 1; k <= '9'; k++ {
+				z01.PrintRune(i)
 				z01.PrintRune(j)
 				z01.PrintRune(k)
-                if i != '7' {
+				if i != '7' {
 					zo1.PrintRune(',')
-                    zo1.PrintRune(' ')
-                }
-            }
-        }
-    }
+					zo1.PrintRune(' ')
+				}
+			}
+		}
+	}
 	z01.PrintRune('\n')
 }
