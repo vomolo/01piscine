4eb72e2a4092a9d808c98b6d7d508199c3777733
vomolo <valentineomolo1@gmail.com>
2023-10-30 16:15:37 +0300

    pick

---
+++ iterativefactorial.go
@@ -0,0 +1,15 @@
+package piscine
+
+func IterativeFactorial(nb int) int {
+	var result int = 1
+	if nb > 63 || nb < -63 {
+		    return 0
+	}
+	for i := 1; i <= nb; i++ {
+		    result = result * i
+	}
+	for i := -1; i >= nb; i-- {
+		    result = result * i
+	}
+	return result
+}
