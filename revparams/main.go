82de7b0e38f83b2cca3ab7043db45bed51c06703
vomolo <valentineomolo1@gmail.comm>
2023-11-02 21:33:14 +0300

    rev

---
+++ revparams/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"os"
+
+	"github.com/01-edu/z01"
+)
+
+func main() {
+	args := os.Args[1:]
+	argsIndex := len(args) - 1
+	for argsIndex >= 0 {
+		z01.PrintRune('\n')
+		for _, char := range args[argsIndex] {
+			z01.PrintRune(char)
+		}
+		argsIndex--
+	}
+}
