a5dd229146cf564999836760ff4cec57a4919ada
vomolo <valentineomolo1@gmail.com>
2023-11-03 05:21:07 +0300

    done

--- revparams/main.go
+++ revparams/main.go
@@ -10,10 +10,10 @@ func main() {
 	args := os.Args[1:]
 	argsIndex := len(args) - 1
 	for argsIndex >= 0 {
-		z01.PrintRune('\n')
 		for _, char := range args[argsIndex] {
 			z01.PrintRune(char)
 		}
 		argsIndex--
+		z01.PrintRune('\n')
 	}
 }
