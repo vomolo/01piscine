eef69c6df8d79671e2eb0d23dbfb005e5c013b72
vomolo <valentineomolo1@gmail.com>
2023-11-09 22:02:51 +0300

    desc

--- descendcomb.go
+++ descendcomb.go
@@ -1,20 +1,23 @@
-package main
+package piscine
 
-import "fmt"
+import (
+	"github.com/01-edu/z01"
+)
 
 func DescendComb() {
 	for i := 99; i >= 10; i-- {
 		for j := i - 1; j >= 10; j-- {
-			fmt.Printf("%02d %02d", i, j)
+			z01.PrintRune(rune(i/10 + '0'))
+			z01.PrintRune(rune(i%10 + '0'))
+			z01.PrintRune(' ')
+			z01.PrintRune(rune(j/10 + '0'))
+			z01.PrintRune(rune(j%10 + '0'))
 
 			if j != 10 {
-				fmt.Print(", ")
+				z01.PrintRune(',')
+				z01.PrintRune(' ')
 			}
 		}
 	}
-	fmt.Println()
-}
-
-func main() {
-	DescendComb()
+	z01.PrintRune('\n')
 }
