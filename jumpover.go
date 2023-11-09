c47bc640bb2fb527a2bf0e9f2201f7a13d6ea796
vomolo <valentineomolo1@gmail.com>
2023-11-09 17:12:57 +0300

    cards

--- jumpover.go
+++ jumpover.go
@@ -4,7 +4,7 @@ func JumpOver(str string) string {
 	if str == "" || len(str) < 3 {
 		return "\n"
 	}
-	var results string
+	var result string
 	for i := 2; i < len(str); i += 3 {
 		result += string(str[i])
 	}
