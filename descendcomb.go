dd82fc8a464e200888362cc262d1f756df35fc86
vomolo <valentineomolo1@gmail.com>
2023-11-09 23:11:47 +0300

    desc

--- descendcomb.go
+++ descendcomb.go
@@ -6,10 +6,11 @@ import (
 
 func DescendComb() {
 	for i := '9'; i >= '0'; i-- {
-		for j := '9' - 1; j >= '0'; j-- {
+		for j := '9'; j >= '0'; j-- {
 			d := j - 1
 			for k := i; k >= '0'; k-- {
 				for ; d >= '0'; d-- {
+
 					z01.PrintRune(i)
 					z01.PrintRune(j)
 					z01.PrintRune(' ')
@@ -17,7 +18,7 @@ func DescendComb() {
 					z01.PrintRune(k)
 					z01.PrintRune(d)
 
-					if i > '0' || k > '0' || d > '0' {
+					if i > '0' || j > '1' || k > '0' || d > '0' {
 						z01.PrintRune(',')
 						z01.PrintRune(' ')
 					}
