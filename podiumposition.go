cd6844904a78ae453408be9d6b146cd47268a11f
vomolo <valentineomolo1@gmail.com>
2023-11-10 00:43:00 +0300

    pod

--- podiumposition.go
+++ podiumposition.go
@@ -1,24 +1,41 @@
 package piscine
 
 func PodiumPosition(podium [][]string) [][]string {
-	positionMap := make(map[string]int)
+	// Create an array of slices to represent the positions
+	positions := [4][]string{}
 
-	for i, place := range podium {
-		if len(place) == 1 {
-			positionMap[place[0]] = i
+	// Iterate over the input podium and place competitors in their respective positions
+	for _, competitor := range podium {
+		position := getPosition(competitor)
+		positions[position-1] = append(positions[position-1], competitor[0])
+	}
+
+	// Convert the array of slices to a slice of slices
+	result := make([][]string, 0, 4)
+	for i := 0; i < 4; i++ {
+		if len(positions[i]) > 0 {
+			result = append(result, positions[i])
 		}
 	}
 
-	competitors := make([]string, len(podium))
-
-	for place, pos := range positionMap {
-		competitors[pos] = place
-	}
-
-	result := make([][]string, len(podium))
-	for i, place := range competitors {
-		result[i] = []string{place}
-	}
-
 	return result
+}
+
+func getPosition(competitor []string) int {
+	if len(competitor) != 1 {
+		return 0
+	}
+
+	switch competitor[0] {
+	case "1st Place":
+		return 1
+	case "2nd Place":
+		return 2
+	case "3rd Place":
+		return 3
+	case "4th Place":
+		return 4
+	default:
+		return 0
+	}
 }
