5b9130a71b8c83c8e303fc46ca7c499ee47d2dc2
vomolo <valentineomolo1@gmail.com>
2023-11-09 14:24:39 +0300

    vbvewbi

--- shoppingsummarycounter.go
+++ shoppingsummarycounter.go
@@ -4,11 +4,11 @@ func ShoppingSummaryCounter(str string) map[string]int {
 	Summary := make(map[string]int)
 	word := ""
 	for i := 0; i < len(str); i++ {
-		if str[i] != '' {
-		word += string(str[i])
+		if str[i] != ' ' {
+			word += string(str[i])
 		} else {
-		Summary[word]++
-		word = ""
+			Summary[word]++
+			word = ""
 		}
 	}
 	Summary[word]++
