223cd522b8ed6e99dcf9da2b9561cf5c46bb5bda
vomolo <valentineomolo1@gmail.com>
2023-11-10 04:55:29 +0300

    max

---
+++ max.go
@@ -0,0 +1,17 @@
+package main
+
+func Max(a []int) int {
+	if len(a) == 0 {
+		return 0
+	}
+
+	maxVal := a[0]
+
+	for _, val := range a {
+		if val > maxVal {
+			maxVal = val
+		}
+	}
+
+	return maxVal
+}
