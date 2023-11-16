d23c89442011d8c9c556c92499c1111a28ec0561
vomolo <valentineomolo1@gmail.com>
2023-11-16 12:32:12 +0300

    level

---
+++ btreeapplybylevel.go
@@ -0,0 +1,25 @@
+package piscine
+
+func BTreeApplyByLevel(root *TreeNode, f func(...interface{}) (int, error)) {
+	if root == nil {
+		return
+	}
+
+	queue := []*TreeNode{root}
+
+	for len(queue) > 0 {
+
+		currentNode := queue[0]
+		queue = queue[1:]
+
+		_, _ = f(currentNode.Data)
+
+		if currentNode.Left != nil {
+			queue = append(queue, currentNode.Left)
+		}
+
+		if currentNode.Right != nil {
+			queue = append(queue, currentNode.Right)
+		}
+	}
+}
